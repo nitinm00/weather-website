0e1f0b4511f41c226389c3bf21246b101a9b8bbf
Nitin Miyyapuram <nitin.miyy@gmail.com>
2020-01-13 03:29:15 -0700

    added changes for heroku deployment

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = search.value
-    const url = 'http://localhost:3000/weather?address=' + location
+    const url = '/weather?address=' + location
 
     msg1.textContent = 'Loading...'
     msg2.textContent = ''
