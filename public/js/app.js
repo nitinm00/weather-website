c17f1f0d698bfd1edeb1d5c9f35f83fc2f189cc2
Nitin Miyyapuram <nitin.miyy@gmail.com>
2020-01-13 02:47:38 -0700

    init commit

---
+++ public/js/app.js
@@ -0,0 +1,38 @@
+console.log('Client side js file is ~loaded~')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const msg1 = document.querySelector('#msg-1')
+const msg2 = document.querySelector('#msg-2')
+
+// msg1.textContent = 'From javascript'
+
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+    const url = 'http://localhost:3000/weather?address=' + location
+
+    msg1.textContent = 'Loading...'
+    msg2.textContent = ''
+    fetch(url).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                // console.log(data.error)
+                msg1.textContent = data.error
+                msg2.textContent = ''
+            }
+            else {
+                msg1.textContent = data.location
+                msg2.textContent = data.forecast
+                // console.log(data.location)
+                // console.log(data.forecast)
+
+            }
+        })
+    })
+    // console.log(location)
+})
+
